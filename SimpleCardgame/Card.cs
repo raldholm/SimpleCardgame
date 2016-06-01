1b3798415b200f91a77b1c8bca65a62454177769
Martin Raldholm <martin@raldholm.se>
2016-06-01 20:35:49 +0200

    First commit

---
+++ SimpleCardgame/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCardgame
+{
+    class Card
+    {
+        private string _value;
+        private string _suit;
+
+        // Konstruktor
+        public Card(string cardValue, string cardSuit)
+        {
+            _value = cardValue;
+            _suit = cardSuit;
+        }
+
+        // ToString Override
+        public override string ToString() { return _value + " of " + _suit; }
+    }
+}
---
+++ SimpleCardgame/Deck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCardgame
+{
+    class Deck
+    {
+        private Card[] deck;
+        private int currentCard;
+        private const int NUMBER_OF_CARDS = 52;
+        private Random ranNum;
+
+        public Deck()
+
+        string[] values =
+        {
+            "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Jack", "Queen", "King"
+        };
+
+        string[] suits = {"Hearts", "Clubs", "Diamonds"}
+    }
+}
