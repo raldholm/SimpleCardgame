15666dfedc8ffd9b61579554a8c45c6992cda8a4
Martin Raldholm <martin@raldholm.se>
2016-06-01 20:42:42 +0200

    #2

--- SimpleCardgame/Deck.cs
+++ SimpleCardgame/Deck.cs
@@ -14,13 +14,22 @@ namespace SimpleCardgame
         private Random ranNum;
 
         public Deck()
-
-        string[] values =
         {
-            "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-            "Jack", "Queen", "King"
-        };
 
-        string[] suits = {"Hearts", "Clubs", "Diamonds"}
+            string[] values =
+            {
+                "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+                "Queen", "King"
+            };
+
+            string[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+            deck = new Card[NUMBER_OF_CARDS];
+            currentCard = 0;
+            ranNum = new Random();
+            for (int count = 0; count < deck.Length; count++)
+            {
+                deck[count] = new Card(values[count % 13], suits[ count / 13]);
+            }
+        }
     }
 }
